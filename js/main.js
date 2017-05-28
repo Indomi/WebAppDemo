e571bf859c70b3672b9d69d18c150289b520cda6
Indomi <UestcSyy@163.com>
2017-05-28 21:43:11 +0800

    更改bug

--- js/main.js
+++ js/main.js
@@ -12,14 +12,15 @@ function getData(){
             "key":"fd64177e9efe6ade8fd18bbca28a3213"
         },
         success:function(data){
-            document.getElementById("text-city").style.display="none";
-            document.getElementById("info").style.display="block";
+            $("#text-city").hide();
+            $("#info").show();
             $("#return-button").show();
             console.log(data.result.today);
+            console.log(Object.keys(data.result.today));
             var temp = 0;
             var obj = data.result.today;
             for(var i in data.result.today){
-                $("#info-more").children().eq(temp).append(obj[i]);
+                $("#info-more").children("p").eq(temp).append(obj[i]);
                 temp++;
             }
         },
@@ -27,8 +28,4 @@ function getData(){
             console.log("不成功");
         }
     });
-}
-function returnFind() {
-    $("#text-city").show();
-    $("#info").hide();
 }
