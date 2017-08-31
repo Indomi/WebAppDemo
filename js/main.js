1c59f10210b3a2ed48fa03babe8974f9f6937acb
indomi <indomi126@gmail.com>
2017-08-31 19:07:32 +0800

    use vue.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,35 @@
 /**
  * Created by asus-pc on 2017/5/28.
  */
-function getData(){
-    $.ajax({
-        type:"GET",
-        url:"http://v.juhe.cn/weather/index",
-        dataType:"jsonp",
-        async:"true",
-        data:{
-            "cityname":document.getElementById("cityname").value,
-            "key":"fd64177e9efe6ade8fd18bbca28a3213"
-        },
-        success:function(data){
-            $("#text-city").hide();
-            $("#info").show();
-            $("#return-button").show();
-            console.log(data.result.today);
-            console.log(Object.keys(data.result.today));
-            var temp = 0;
-            var obj = data.result.today;
-            for(var i in data.result.today){
-                $("#info-more").children("p").eq(temp).append(obj[i]);
-                temp++;
-            }
-        },
-        error:function () {
-            console.log("不成功");
-        }
-    });
-}
+ function success(text){
+    document.getElementById("text-city").style.display="none";
+    document.getElementById("info").style.display="block";
+    var app = new Vue({
+    el: '#info-more',
+    data: {
+        temp:text.result.sk.temp,
+        wd:text.result.sk.wind_direction,
+        ws:text.result.sk.wind_strength,
+        humidity:text.result.sk.humidity,
+        time:text.result.sk.time,
+        city:text.result.today.city,
+        temper:text.result.today.temperature,
+        wea:text.result.today.weather,
+        di:text.result.today.dressing_index,
+        da:text.result.today.dressing_advice,
+        uv:text.result.today.uv_index,
+        ci:text.result.today.comfort_index,
+        wi:text.result.today.wash_index,
+        ti:text.result.today.travel_index,
+        ei:text.result.today.exercise_index,
+        di:text.result.today.drying_index
+    }
+})
+    console.log(text);
+ }
+ function createData(){
+    var jsonp = document.createElement("script");
+    head = document.getElementsByTagName("head")[0];
+    jsonp.src = "http://v.juhe.cn/weather/index?callback=success&key=fd64177e9efe6ade8fd18bbca28a3213&cityname="+document.getElementById("cityname").value;
+    head.appendChild(jsonp);
+ }
